70d823d2b898ce48107c3fb1cc210a3a236fa221
nikeru8 <nikeru888@gmail.com>
2016-05-18 20:58:53 +0800

    第一版:照相功能+隨機換圖

---
+++ app/src/main/java/com/example/nikeru8/myapplication/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.nikeru8.myapplication;
+
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View m_view_food;
+    private TextView m_food_name;
+    private TextView m_view_message;
+    private Button m_btn_start;
+    private TypedArray mFoodImage;//資源檔陣列
+    private int mFoodImageCount;//一共多少張圖片
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();//初始畫面佈局連結
+        initFoodImage();
+    }
+
+    private void initFoodImage() {
+        mFoodImage = getFoodImage();
+        mFoodImageCount = getFoodImage().length();
+        m_view_food.setBackground(mFoodImage.getDrawable(0));
+
+    }
+
+    private TypedArray getFoodImage() {
+        //取得FOOD IMAGE drawables
+        TypedArray Foods = getResources().obtainTypedArray(R.array.food_images);
+        return Foods;
+
+    }
+
+    private void initView() {
+        m_view_food = findViewById(R.id.tv_food_image);
+        m_food_name = (TextView) findViewById(R.id.tv_food_name);
+        m_view_message = (TextView) findViewById(R.id.view_message);
+        m_btn_start = (Button) findViewById(R.id.btn_start);
+    }
+
+
+    //Handler 處理代辦事項
+    private Handler m_Handler = new Handler();
+    //建立任務物件
+
+    public void start(View view) {
+        m_Handler.post(mStartRandomTask);//立即執行任務
+        m_Handler.postDelayed(mStopRandomTask, 3000);//3秒後執行任務 停止隨機換圖
+        m_btn_start.setEnabled(false);//案下去之後,按鈕失效變灰色
+
+    }
+
+    //建立任務物件
+    private StartRandomTask mStartRandomTask = new StartRandomTask();
+    private StopRandomTask mStopRandomTask = new StopRandomTask();
+
+
+    private class StartRandomTask implements Runnable {
+
+        @Override
+        public void run() {
+            //隨機產生0~圖數量-1
+            int index = (int) (Math.random() * mFoodImageCount);
+            //換圖
+            m_view_food.setBackground(mFoodImage.getDrawable(index));
+            //0.1秒後再執行一次本任務
+            m_Handler.postDelayed(this, 100);
+        }
+    }
+
+    private class StopRandomTask implements Runnable {
+        @Override
+        public void run() {
+            //取消任務startRandomTask
+            m_Handler.removeCallbacks(mStartRandomTask);
+            //start按鈕變成可按
+            m_btn_start.setEnabled(true);
+
+        }
+    }
+
+
+    //拍照活動按鈕
+    public void ActivityForPhoto(View view) {
+        Intent intent = new Intent(this, photo.class);
+        startActivity(intent);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/nikeru8/myapplication/photo.java
@@ -0,0 +1,70 @@
+package com.example.nikeru8.myapplication;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class photo extends AppCompatActivity {
+    private LinearLayout layout;
+    final static private int ShotPhoto = 0;
+
+    private ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+        init();
+    }
+
+    public void init() {
+        imageView = (ImageView) findViewById(R.id.Photo_Place);
+layout=(LinearLayout)findViewById(R.id.tv_layout);
+    }
+
+    public void onclick(View view) {
+
+        //建立一個意圖(內建方法)
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //開始並回傳意圖
+        startActivityForResult(intent, ShotPhoto);
+    }
+
+
+    //回傳意圖的地方，用此方法回傳意圖
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ShotPhoto && resultCode == RESULT_OK) {
+            //Bitmap點陣圖
+            //取得資源>>Data.getExtras().get("data");在轉型成Bitmap
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            //設定點陣圖片
+            imageView.setImageBitmap(bitmap);
+            //設定圖片最適化
+            imageView.setAdjustViewBounds(true);
+
+
+
+            //控管下面的scroll
+            ImageView imageview2=new ImageView(this);
+            //設定點陣圖到imageView2中
+            imageview2.setImageBitmap(bitmap);
+            //設定最適合大小
+            imageview2.setAdjustViewBounds(true);
+            //每拍一張新增一張
+            layout.addView(imageview2);
+
+
+
+        }
+
+    }
+
+}
